091a04e45847847697417efbc69f8ad6a582a65f
Vova00 <mr.vproen7shchsgjg@gmail.com>
2021-08-10 18:22:38 +0300

    тест комит

--- app/src/main/java/com/example/fontnnes/Home.java
+++ app/src/main/java/com/example/fontnnes/Home.java
@@ -14,6 +14,7 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
+
     public void Open_message(View view) {
         Intent button = new Intent(this, Message.class);
         startActivity(button);
